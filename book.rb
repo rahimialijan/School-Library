8c16197d01c6b6b9b05501f974d5828ce8fcbd97
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-02 21:11:15 +0500

    setup associations between class and obj and fix all linter errors

---
+++ book.rb
@@ -0,0 +1,16 @@
+require_relative 'rental'
+
+class Book
+  attr_accessor :title, :author, :rental
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rental = []
+  end
+
+  def add_rental(rental)
+    @rental.push(rental)
+    rental.book = self
+  end
+end
