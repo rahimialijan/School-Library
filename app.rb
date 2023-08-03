cbd24667c9e6b9842a639a48c951e2a492c12b48
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-03 18:49:00 +0500

    Add basic UI for school-library PR

---
+++ app.rb
@@ -0,0 +1,94 @@
+# app.rb
+
+require_relative 'classroom'
+require_relative 'book'
+require_relative 'nameable'
+require_relative 'person'
+require_relative 'rental'
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'decorator'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+  def list_books
+    puts 'List of Books:'
+    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    puts
+  end
+
+  def list_people
+    puts 'List of People:'
+    @people.each { |person| puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}" }
+    puts
+  end
+
+  def create_person(name, age, type, specialization = nil)
+    if type == 'teacher'
+      person = Teacher.new(specialization, name, age)
+    elsif type == 'student'
+      person = Student.new(name, age)
+    else
+      puts 'Invalid person type.'
+      return
+    end
+
+    @people << person
+    puts "Person with ID #{person.id} was created."
+  end
+
+  def create_book(title, author)
+    book = Book.new(title, author)
+    @books << book
+    puts "Book '#{book.title}' by #{book.author} was created."
+  end
+
+  def create_rental
+    puts 'Enter the rental date (yyyy-mm-dd):'
+    date = gets.chomp
+    puts 'Enter the book title for the rental:'
+    title = gets.chomp
+
+    book = @books.find { |b| b.title == title }
+    if book.nil?
+      puts "Book with title '#{title}' not found."
+      return
+    end
+
+    puts 'Enter the person ID for the rental:'
+    person_id = gets.chomp.to_i
+
+    person = @people.find { |p| p.id == person_id }
+    if person.nil?
+      puts "Person with ID #{person_id} not found."
+      return
+    end
+
+    rental = Rental.new(date, book, person)
+    @rentals << rental
+
+    puts "Rental of '#{book.title}' by #{book.author} to #{person.correct_name} on #{rental.date} has been created."
+  end
+
+  def list_rentals_for_person(person_id)
+    person = @people.find { |p| p.id == person_id }
+
+    if person.nil?
+      puts 'Person not found.'
+      return
+    end
+
+    puts "Rentals for #{person.name}:"
+    person.rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: #{rental.book.title}, Author: #{rental.book.author}"
+    end
+    puts
+  end
+end
--- main.rb
+++ main.rb
@@ -1,11 +1,88 @@
-require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+# main.rb
 
-person = Person.new('maximilianus', 22)
+require_relative 'app'
 
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+def main_menu
+  puts '------------------------'
+  puts 'Welcome to the Library!'
+  puts '------------------------'
+  puts 'Please choose an option:'
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List rentals for a person'
+  puts '7. Quit'
+  print 'Enter your choice: '
+  gets.chomp.to_i
+end
+
+def create_person(app)
+  print 'Enter name: '
+  name = gets.chomp
+  print 'Enter age: '
+  age = gets.chomp.to_i
+  print 'Enter person type (teacher or student): '
+  type = gets.chomp.downcase
+  if type == 'teacher'
+    print 'Enter specialization: '
+    specialization = gets.chomp
+    app.create_person(name, age, type, specialization)
+  elsif type == 'student'
+    app.create_person(name, age, type)
+  else
+    puts 'Invalid person type.'
+  end
+end
+
+def create_book(app)
+  print 'Enter book title: '
+  title = gets.chomp
+  print 'Enter book author: '
+  author = gets.chomp
+  app.create_book(title, author)
+end
+
+def create_rental(app)
+  app.create_rental
+end
+
+def list_rentals(app)
+  print 'Enter person ID: '
+  person_id = gets.chomp.to_i
+  app.list_rentals_for_person(person_id)
+end
+
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength
+def main
+  app = App.new
+
+  loop do
+    choice = main_menu
+
+    case choice
+    when 1
+      app.list_books
+    when 2
+      app.list_people
+    when 3
+      create_person(app)
+    when 4
+      create_book(app)
+    when 5
+      create_rental(app)
+    when 6
+      list_rentals(app)
+    when 7
+      puts 'Goodbye!'
+      break
+    else
+      puts 'Invalid choice. Please try again.'
+    end
+  end
+end
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/MethodLength
+
+# Call the main method to start the app
+main
